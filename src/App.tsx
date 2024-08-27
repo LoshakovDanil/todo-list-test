6eb6ad3ce6d557efa9479115cd7186351653fd22
Loshakov Danil <loshakovdanil9@gmail.com>
2024-08-27 21:07:43 +0300

    dev: add new text

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        my App
+      </div>
       <div className="container">
         <Todo />
       </div>
